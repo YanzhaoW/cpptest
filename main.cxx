ab4ee4adfd9de96e89088fa55787f8b62eb726b2
YanzhaoW <yannzhaow@gmail.com>
2023-04-05 13:13:37 +0200

    merge_test

--- main.cxx
+++ main.cxx
@@ -1,12 +1,20 @@
+#include "UseRoot.hpp"
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TH1D.h>
+#include <TROOT.h>
+#include <TRandom3.h>
+#include <TTree.h>
 #include <array>
 #include <chrono>
 #include <iostream>
 #include <string>
 #include <vector>
 
+auto func(int num = 10) -> int { return num; }
+
 auto main() -> int
 {
-    auto vec = std::vector<int>{};
-    double arr[3] = {};
+    auto inte = func(10);
     return 0;
 }
